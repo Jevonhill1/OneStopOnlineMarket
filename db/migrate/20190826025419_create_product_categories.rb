056f1e67abe1a7c40f40a66c761f040ee577483b
Jevon Hill <jevon_hill@hotmail.com>
2019-08-26 18:31:55 -0400

    changes to schema

--- db/migrate/20190826025419_create_product_categories.rb
+++ db/migrate/20190826025419_create_product_categories.rb
@@ -1,7 +1,7 @@
 class CreateProductCategories < ActiveRecord::Migration[6.0]
   def change
     create_table :product_categories do |t|
-      t.string :product
+      t.string :product, null: false, foriegn_key: true
       t.string :belongs_to
       t.belongs_to :category, null: false, foreign_key: true
 
