df41780c3221b854e86bc09790d558c3f4444215
Jevon Hill <jevon_hill@hotmail.com>
2019-09-28 20:19:58 -0400

    restarting the project

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 class CategoriesController < ApplicationController
   def index
-    @category = category.order(:title)
+    @categories = Category.order(:title)
+    # @categories = Category.joins(:products).select('categories.*, count(products.id) as products_count').group('categories.id').order(:title)
   end
 
 
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
 
   has_many :product_categories
-  has_many :categories, through: :product_categories 
+  has_many :categories, through: :product_categories
+  has_and_belongs_to_many :products 
 end
