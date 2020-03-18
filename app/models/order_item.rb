f4975aefed407573ef18eaa548d91ab5a145e676
Jevon Hill <1jevonhill@gmail.com>
2020-03-18 02:41:46 -0400

    first commit

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,5 +2,7 @@ class OrderItem < ApplicationRecord
 
   belongs_to :order
   belongs_to :product
-  
+  has_many :items, class_name: 'OrderItem'
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_09_02_182115) do
   create_table "categories_products", id: false, force: :cascade do |t|
     t.bigint "category_id", null: false
     t.bigint "product_id", null: false
+    t.index ["category_id", "product_id"], name: "index_categories_products_on_category_id_and_product_id"
     t.index ["product_id", "category_id"], name: "index_categories_products_on_product_id_and_category_id"
   end
 
