56e827a5f380b548ea5accc49e752db5f3533209
Jevon Hill <1jevonhill@gmail.com>
2021-02-19 03:20:44 -0500

    add user table and user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,6 @@ product = Product.create([
   product_thumbnail: File.open(Rails.root.join('public', 'images', 
   'Atlantis-Flower---Bud_500x.png'))
   
-  product_company: 'Hexo',
   product_category: "Dried Flowers"
 }
 ])
